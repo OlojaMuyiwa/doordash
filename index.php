ad241dbc1f18b53d490d874438b94d79d9b9634d
OlojaMuyiwa <olojam4969@gmail.com>
2022-12-17 17:54:43 +0100

    commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud Application</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="icon" type="image/x-icon" href="../websiteplanet.com-favicons//favicons//favicon-16x16.png">
+</head>
+<body class="p-4">
+  <h2 class="text-center text-primary">Products Crud</h2>
+    <button class="btn btn-success text-center mx-auto d-flex justify-center"><a href="create.php" class="text-decoration-none text-light">Create Products</a></button>
+<table class="table py-5">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Image</th>
+      <th scope="col">Product</th>
+      <th scope="col">Create-Date</th>
+      <th scope="col">Price</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>
+      <button type="button" class="btn btn-outline-warning btn-sm" >Edit</button>
+      <button type="button" class="btn btn-outline-danger btn-sm">Delete</button>
+      </td>
+    </tr>
+    <tr>
+      
+     
+    </tr>
+    <tr>
+      
+     
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
